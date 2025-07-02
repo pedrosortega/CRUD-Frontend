10e8bfd41f5625b9dbe42bb1a835a4791f8e8074
tabularasae <tabularasae@icloud.com>
2025-07-01 20:39:57 -0400

    Created the Routes inside App.jsx  and itialized the components as per project requirments

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,93 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
+import axios from "axios";
 import "./AppStyles.css";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Routes } from "react-router";
+import api from "./api/axiosInstance";
+import HomePage from "./components/HomePage";
+import CampusesList from "./components/CampusesList";
+import CampusForm from "./components/CampusForm";
+import SingleCampus from "./components/SingleCampus";
+import StudentsList from "./components/StudentsList";
+import StudentForm from "./components/StudentForm";
+import SingleStudent from "./components/SingleStudent";
+import NotFound from "./components/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
 const App = () => {
+  const [students, setStudents] = useState([]);
+  const [campuses, setCampuses] = useState([]);
+
+  async function fetchAllStudents() {
+    try {
+      const StudentResponse = await api.get(`/students`);
+      console.log("✅ API StudentResponse:", StudentResponse.data);
+      setStudents(StudentResponse.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  }
+
+  async function fetchAllCampuses() {
+    try {
+      const CampusResponse = await api.get(`/campuses`);
+      console.log("✅ API CampusResponse:", CampusResponse.data);
+      setCampuses(CampusResponse.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllCampuses();
+    fetchAllStudents();
+  }, []);
+
   return (
     <div>
+      <h1>Campus Listing</h1>
       <NavBar />
       <div className="app">
-        <h1>Hello React!</h1>
-        <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-        <Routes>{/* Currently, we don't have any routes defined */}</Routes>
+          <Route
+            path="/campuses"
+            element={<CampusesList campuses={campuses} />}
+          />
+
+          <Route
+            path="/campuses/new"
+            element={<CampusForm mode="create" onSuccess={fetchAllCampuses} />}
+          />
+
+          <Route
+            path="/campuses/:id/edit"
+            element={<CampusForm mode="edit" onSuccess={fetchAllCampuses} />}
+          />
+
+          <Route path="/campuses/:id" element={<SingleCampus />} />
+
+          <Route
+            path="/students"
+            element={<StudentsList students={students} />}
+          />
+
+          <Route
+            path="/students/new"
+            element={<StudentForm mode="create" onSuccess={fetchAllStudents} />}
+          />
+
+          <Route
+            path="/students/:id/edit"
+            element={<StudentForm mode="edit" onSuccess={fetchAllStudents} />}
+          />
+
+          <Route path="/students/:id" element={<SingleStudent />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./NavBarStyles.css";
+import {NavLink} from "react-router";
 
 const NavBar = () => {
-  return <nav className="navbar">{/* Some navbar stuff goes here */}</nav>;
+  return ( <nav className="navbar">
+  <NavLink id="home-button" to="/"> Home </NavLink>
+  <NavLink to="/campuses"> Campuses </NavLink>
+  <NavLink to="/students"> Students </NavLink>
+  </nav>
+  );
 };
 
 export default NavBar;
