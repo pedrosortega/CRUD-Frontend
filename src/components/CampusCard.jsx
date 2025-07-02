4fee2841242d124bbc8d2f2edf148866eb8601ba
pedrosortega <psortega2004@gmail.com>
2025-07-02 12:52:32 -0400

    implementing all campuses

---
+++ src/components/CampusCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import api from "../api/axiosInstance";
+
+const CampusCard = ({ campus, fetchAllCampuses }) => {
+  const Delete = async () => {
+    try {
+      await api.delete(`/campuses/${campus.id}`);
+      fetchAllCampuses();
+    } catch (e) {
+      console.error("Error deleting student: ", e);
+    }
+  };
+  return (
+    <div className="campus-card">
+      <img
+        src={campus.imageURL}
+        alt={`image of ${campus.name}`}
+        width={125}
+        height={125}
+      />
+      <div className="campus-info">
+        <h3>{campus.name}</h3>
+      </div>
+    </div>
+  );
+};
--- src/components/CampusesList.jsx
+++ src/components/CampusesList.jsx
@@ -1,4 +1,16 @@
-// to do
-
+import React from "react";
+const CampusesList = ({ campuses = [], fetchAllCampuses }) => {
+  return (
+    <div>
+      {campuses.map((c) => (
+        <CampusCard
+          key={c.imageUrl}
+          campus={c}
+          fetchAllCampuses={fetchAllCampuses}
+        />
+      ))}
+    </div>
+  );
+};
 // This goes at the bottom of the file
 // export default CampusesList;
