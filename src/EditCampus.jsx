6ea66d28f3ba2bd493a6e26720f7b50d2161ecae
rend1027 <rend1027@yahoo.com>
2025-07-04 13:11:30 -0400

    created edit campus form

--- src/EditCampus.jsx
+++ src/EditCampus.jsx
@@ -3,8 +3,36 @@ import "./EditCampus.css"
 
 const EditCampus = () => {
     return (
-        <main>
-            Test Edit Campus
+        <main className='edit-campus-container'>
+            <h1>Edit Campus </h1>
+            <form>
+                <label>Campus Name:</label>
+                <input
+                type='text'
+                name='name'
+                value=''
+                required
+                />
+                <label>Image:</label>
+                <input
+                type='text'
+                name='img'
+                value=''
+                />
+                <label>Description:</label>
+                <textarea
+                type='text'
+                name='description'
+                value=''
+                rows={10}
+                cols={50}
+                />
+                <label>Address:</label>
+                <input
+                type='text'
+                name='address'
+                value=''/>
+            </form>
         </main>
     )
 }
