4a6000abb9cbd4b047d853a95bb0c81102156bca
tabularasae <tabularasae@icloud.com>
2025-07-06 17:04:18 -0400

    changing to the permanent link

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: `https://crud-backend-jd3px0mnu-markfartushniaks-projects.vercel.app/api`,
+    baseURL: `https://crud-backend-eta-fawn.vercel.app/api`,
 });
 
 export default api;
